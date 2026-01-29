624d729c3ff0937c640972a06d530af7f894a396
Dang Nguyen <nguyen05082004@gmail.com>
2026-01-30 01:00:39 +0700

    use api of server in constant

--- Common/Constants/ApiConstant.cs
+++ Common/Constants/ApiConstant.cs
@@ -2,8 +2,8 @@
 {
     public static class ApiConstant
     {
-        public static string apiBaseUrl = "https://elearningtoanhochay-production.up.railway.app";
-        //public static string apiBaseUrl = "http://103.98.152.182";
+        //public static string apiBaseUrl = "https://elearningtoanhochay-production.up.railway.app";
+        public static string apiBaseUrl = "http://103.98.152.182";
         public static string webBaseUrl = "https://www.toanhochay.com";
         //public static string apiBaseUrl = "https://localhost:7290";
     }
