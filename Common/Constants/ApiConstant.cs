0433a2931189f59cd9ea45a31d79b8f12c9a0e03
ducnt14112004 <ducnthe180869@fpt.edu.vn>
2026-02-01 18:55:18 +0700

    sua apiconstant

--- Common/Constants/ApiConstant.cs
+++ Common/Constants/ApiConstant.cs
@@ -3,7 +3,7 @@
     public static class ApiConstant
     {
         //public static string apiBaseUrl = "https://elearningtoanhochay-production.up.railway.app";
-        public static string apiBaseUrl = "https://localhost:7290";
+        public static string apiBaseUrl = "http://103.98.152.182";
         public static string webBaseUrl = "https://www.toanhochay.com";
         //public static string apiBaseUrl = "https://localhost:7290";
     }
